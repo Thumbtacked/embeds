134e030c6bd022cc56769d29cb53ee3104083e31
ilovetocode <54194229+ilovetocode2019@users.noreply.github.com>
2024-10-14 23:46:16 -0700

    Handle errors properly

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 import aiohttp
+import aiohttp_cors
 import yaml
 
 from aiohttp import web
 from lxml import etree
 
-@web.middleware
-async def default_headers(request, handler):
-    response = await handler(request)
- 
-    if response is not None:
-        response.headers.setdefault("Access-Control-Allow-Origin", "*")
-        response.headers.setdefault("Access-Control-Allow-Headers", "*")
-        response.headers.setdefault("Access-Control-Allow-Methods", "*")
-        return response
 
 async def health(request):
     return web.Response(text="Alive and healthy.")
@@ -24,26 +16,41 @@ async def process(request):
         async with aiohttp.ClientSession() as session:
             resp = await session.get(url)
             html = await resp.text()
-            root = etree.fromstring(html, etree.HTMLParser())
-    except aiohttp.InvalidUrlClientError:
-        return web.json_response({"error": "Unable to reach URL."})
+    except aiohttp.ClientConnectionError:
+        return web.json_response({"error": "Unable to reach URL."}, status=400)
+
+    if not resp.headers.get("Content-Type", "").startswith("text/html"):
+        return web.json_response({"error": "The provided URL is not an HTML document."}, status=400)
+
+    root = etree.fromstring(html, etree.HTMLParser())
 
     title = root.xpath(".//title")
     description = root.xpath(".//meta[@name='description']")
     image = root.xpath(".//meta[@property='og:image']")
-    icon = root.xpath(".//link[@rel='icon]")
+
 
     return web.json_response({
+        "url": url,
         "title": title[0].text if title else None,
-        "description": description[0].get("content") if description else None,
+        "description": description[0].get("content")[:150] + ("..." if len(description[0].get("content")) > 150 else "") if description else None,
         "image": image[0].get("content") if image else None,
         "error": None
     })
 
-server = web.Application(middlewares=[default_headers])
-server.add_routes([web.get("/", health)])
-server.add_routes([web.post("/process", process)])
+server = web.Application()
+server.router.add_get("/", health)
+server.router.add_post("/process", process)
 
+cors = aiohttp_cors.setup(server, defaults={
+   "*": aiohttp_cors.ResourceOptions(
+        allow_credentials=True,
+        expose_headers="*",
+        allow_headers="*"
+    )
+})
+
+for route in list(server.router.routes()):
+    cors.add(route)
 
 with open("config.yaml") as f:
     config = yaml.safe_load(f) or {}
